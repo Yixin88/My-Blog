3f7627abcd6c0a139a9ff757690df06977871ad8
Yixin <yixin88@hotmail.co.uk>
2023-06-01 22:20:11 +0100

    added logo component and styles

---
+++ components/layout/logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './logo.module.css'
+
+export default function Logo() {
+  return (
+    <div className={styles.logo}>
+      Keeping up with Web Development
+    </div>
+  )
+}
