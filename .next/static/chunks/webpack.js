fd90bf4fbae05e16b87de86781954da5363a2c8f
Yixin <yixin88@hotmail.co.uk>
2023-06-02 15:13:06 +0100

    added featured post with some dummy date for now

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb78d55217b02396"; }
+/******/ 		__webpack_require__.h = function() { return "ad6e68b504667d37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,42 @@ import Hero from '../components/home-page/hero'
 import FeaturedPosts from '../components/home-page/featured-posts'
 
 export default function HomePage() {
+
+  const dummyPost = [
+    {
+      slug: 'getting-started-with-nextjs',
+      title: 'Getting started with NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+      date: '2023-02-23'
+    },
+    {
+      slug: 'getting-started-with-nextjs2',
+      title: 'Getting started with NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+      date: '2023-02-23'
+    },
+    {
+      slug: 'getting-started-with-nextjs3',
+      title: 'Getting started with NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+      date: '2023-02-23'
+    },
+    {
+      slug: 'getting-started-with-nextjs4',
+      title: 'Getting started with NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+      date: '2023-02-23'
+    },
+  ]
+
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={dummyPost}/>
     </>
   )
 }
