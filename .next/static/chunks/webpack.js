7f1dc30fa747f9effa30a67501bc74ee53fe761e
Yixin <yixin88@hotmail.co.uk>
2023-06-03 22:56:06 +0100

    changed getting started with nextjs blog, and changed custom img tag output from md file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "815230a767a531a4"; }
+/******/ 		__webpack_require__.h = function() { return "60bbcd296d8903a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,19 +1,55 @@
-import React from 'react'
-import PostHeader from './post-header'
-import ReactMarkdown from 'react-markdown'
+import React from "react";
+import PostHeader from "./post-header";
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
-import styles from './post-content.module.css'
+import styles from "./post-content.module.css";
 
 export default function PostContent(props) {
-
   const { post } = props;
 
-  const imagePath = `/images/posts/${post.slug}/${post.image}`
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = { //this is to override react markdown and give your own html to element
+    // img(image) { // this is for all img element in md, instead of regular img tag, it uses nextjs img optimised tag
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+
+    p(paragraph) { // this is to override paragraph element including image wrapped in p tag, since its the default for md
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') { // we are specific targeting img element that is wrapped in p tag
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        )
+      }
+
+      return (
+        <p>{paragraph.children}</p> // if theres no image wrapped in p tag, then its a normal p tag element
+      )
+    }
+  };
 
   return (
     <article className={styles.content}>
-      <PostHeader title={post.title} image={imagePath}/>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
-  )
+  );
 }
