14e1cc6d5245e6393dcb5409851e0d8b5909a8ed
Yixin <yixin88@hotmail.co.uk>
2023-06-04 21:36:00 +0100

    added contact form page and styles, next is to hook it with api route

--- .next/static/webpack/webpack.4836beb1b6fbbe75.hot-update.js
+++ .next/static/webpack/webpack.4836beb1b6fbbe75.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "10df554fadb780ea"; }
+/******/ 	__webpack_require__.h = function() { return "6b77cef5eb888f56"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
