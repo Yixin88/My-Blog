ce5ca397abfc260181267024e3c549c65da8cbe2
Yixin <yixin88@hotmail.co.uk>
2023-06-04 22:08:28 +0100

    contact form inputs are now hook to api route

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,30 +1,46 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-import styles from './contact-form.module.css'
+import styles from "./contact-form.module.css";
 
 export default function ContactForm() {
+  const [ enteredEmail, setEnteredEmail ] = useState('');
+  const [ enteredName, setEnteredName ] = useState('');
+  const [ enteredMessage, setEnteredMessage ] = useState('');
+
+  function sendMessageHandler(e) {
+    e.preventDefault();
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
   return (
     <section className={styles.contact}>
       <h1>How can I help you?</h1>
-      <form className={styles.form}>
-
+      <form className={styles.form} onSubmit={sendMessageHandler}>
         <div className={styles.controls}>
-
           <div className={styles.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id='email' required/>
+            <input type="email" id="email" value={enteredEmail} onChange={e => setEnteredEmail(e.target.value)} required />
           </div>
 
           <div className={styles.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id='name' required/>
+            <input type="text" id="name" value={enteredName} onChange={e => setEnteredName(e.target.value)} required />
           </div>
-
         </div>
 
         <div className={styles.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea id="message" rows="5" value={enteredMessage} onChange={e => setEnteredMessage(e.target.value)} required></textarea>
         </div>
 
         <div className={styles.actions}>
@@ -32,5 +48,5 @@ export default function ContactForm() {
         </div>
       </form>
     </section>
-  )
+  );
 }
