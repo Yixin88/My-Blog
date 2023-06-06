5af911f6961aec0ba8cdecc69cd7bbabb31c81ef
Yixin <yixin88@hotmail.co.uk>
2023-06-06 15:28:57 +0100

    added global head data in _app.js for device viewport

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57dbb6895886adf4"; }
+/******/ 		__webpack_require__.h = function() { return "d63fb12d2d4d5bc8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
