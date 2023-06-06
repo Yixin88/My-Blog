944e203fb6268e52c8395eab4a1be728fab549eb
Yixin <yixin88@hotmail.co.uk>
2023-06-06 15:32:53 +0100

    added head and meta tag on home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Head from 'next/head'
+
 import Hero from '../components/home-page/hero'
 import FeaturedPosts from '../components/home-page/featured-posts'
 import { getFeaturedPost } from '../lib/posts-util'
@@ -7,6 +9,10 @@ export default function HomePage(props) {
 
   return (
     <>
+      <Head>
+        <title>Yixin's Blog</title>
+        <meta name='description' content='Keeping up with web development news and programming.'/>
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts}/>
     </>
