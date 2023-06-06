683e87915245bbda1d597f8c681e46fd2010b4a2
Yixin <yixin88@hotmail.co.uk>
2023-06-06 15:34:50 +0100

    added head and meta tag on contact page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d63fb12d2d4d5bc8"; }
+/******/ 		__webpack_require__.h = function() { return "9122c6860fb8802d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Head from 'next/head'
+
 import ContactForm from '../../components/contact/contact-form'
 
 export default function ContactPage() {
   return (
-    <ContactForm />
+    <>
+      <Head>
+        <title>Contact me</title>
+        <meta name='description' content='Send me your messages!'/>
+      </Head>
+      <ContactForm />
+    </>
   )
 }
