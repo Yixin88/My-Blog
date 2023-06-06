48a8eb32d685752111ff3084d202044d5ac14f8d
Yixin <yixin88@hotmail.co.uk>
2023-06-06 15:54:29 +0100

    added lang setting for site in _document.js file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a888f1c3e28cd9d2"; }
+/******/ 		__webpack_require__.h = function() { return "988672bb0f422e38"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
