dfff60be4ad04d43ed28a5cd261e824056e62f5c
Yixin <yixin88@hotmail.co.uk>
2023-06-06 15:48:03 +0100

    added head and meta tag on single post page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Head from 'next/head';
+
 import PostContent from '../../components/posts/post-detail/post-content'
 import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
 export default function SinglePostPage(props) {
   return (
-    <PostContent post={props.post}/>
+    <>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta name='description' content={props.post.excerpt}/>
+      </Head>
+      <PostContent post={props.post}/>
+    </>
   )
 }
 
